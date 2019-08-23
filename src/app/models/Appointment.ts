1ef4771138bf32259867abd5f3a30582766b6f14
Diogo Machado <diogomachado_8@hotmail.com>
2019-08-23 10:13:38 -0300

    Migration e model de agendamento

---
+++ src/app/models/Appointment.ts
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  date: Date;
+  canceled_at: Date;
+  static init(sequelize): typeof Appointment {
+    //@ts-ignore
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models): void {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
